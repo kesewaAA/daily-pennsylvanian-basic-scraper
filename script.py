3549aacc0562cfeb177d205fad9a5086fcb3a1c0
Lisa Kesewa Ayisi-Ahwireng <163767660+kesewaAA@users.noreply.github.com>
2025-05-14 00:03:07 -0400

    Update script.py

--- script.py
+++ script.py
@@ -30,7 +30,7 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        headline_tag = soup.find("h3", class_="standard-link")  
+        headline_tag = soup.select_one("h3.standard-link > a") 
         if headline_tag:
             headline = headline_tag.get_text(strip=True)
             loguru.logger.info(f"Opinion headline: {headline}")
